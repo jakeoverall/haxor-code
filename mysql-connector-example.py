f37466e699259aeb097e761288515b90503f4f0f
Jake Overall <joverall22@gmail.com>
2022-03-10 12:36:26 -0700

    send email

--- mysql-connector-example.py
+++ mysql-connector-example.py
@@ -9,15 +9,42 @@ connection = mysql.connector.connect(
     database="codeworks"
 )
 
+
 def get_data(table):
-  print(f'--------{table}---------')
-  cursor = connection.cursor()
-  cursor.execute(f"SELECT * FROM {table}")
-  data = cursor.fetchall()
-  for record in data:
-      print(record)
-  print('\n')
+    print(f'--------{table}---------')
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table}")
+    data = cursor.fetchall()
+    for record in data:
+        print(record)
+    print('\n')
 
 
-get_data('users')
-get_data('courses')
+def get_single_item(table, id):
+    print(f'--------{table}---------')
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table} WHERE id = {id}")
+    data = cursor.fetchall()
+    print(data)
+    print('\n')
+
+
+def get_user_account(username, password):
+    print(f'--------GETTING USER ACCOUNT---------')
+    cursor = connection.cursor()
+    cursor.execute(
+        f"SELECT * FROM users WHERE password = {password} AND email = '{username}';")
+    data = cursor.fetchall()
+    print(data)
+    print('\n')
+
+
+get_user_account('jake@boisecodeworks.com', "'x' OR 1=1")
+# get_user_account('t@test.com', 'test')
+
+
+# get_data('users')
+# get_data('courses')
+
+
+# get_single_item('courses', '2 OR 1=1')
--- uptime-monitor.py
+++ uptime-monitor.py
@@ -1,10 +1,29 @@
 import datetime
 import os
+import smtplib
+import ssl
 from time import sleep
 
 host = input('What is the IP? ')
 delay = input('How often do you want to check the host (seconds)? ')
-emailTo = input('In the event that the server goes down what email would you like to have notified?')
+emailTo = input(
+    'In the event that the server goes down what email would you like to have notified?')
+password = input('Please supply your email password. ')
+port = 465
+context = ssl.create_default_context()
+
+
+def send_email(subject, message):
+    with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+        server.login(emailTo, password)
+        server.sendmail(emailTo, emailTo, f"""From: {emailTo}
+Subject: {subject}
+
+{message}
+""")
+        server.close()
+        print("Email Sent")
+
 
 # how do I call a function every x seconds in python?
 # how do I convert a str into a number?
@@ -18,9 +37,12 @@ def send_ping_request():
     if response == 0:
         # Write response to a file
         file.write(f'UP: {status}')
+        # send_email('[ITS ALL GOOD]', status)
     else:
         # TODO send an email when the server is down
         file.write(f'DOWN:  {status}')
+        send_email('[SERVER IS DOWN]', status)
+        exit(1)
     sleep(int(delay))
     send_ping_request()
 
