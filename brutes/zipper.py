421ebfc0a7a6c5b60506323590028ec01212581d
Jake Overall <joverall22@gmail.com>
2022-03-31 12:03:18 -0600

    completed

--- brutes/zipper.py
+++ brutes/zipper.py
@@ -1,10 +1,20 @@
+from time import sleep
 from zipfile import ZipFile
 
 
 def start():
     filepath = input('Zipfile: ')
     extract_location = input('where would you like to extract this to?')
-    # TODO get the file list and try each password in the list 
+    # TODO get the file list and try each password in the list
+    f = open('./passlist', 'r')
+    passwords = f.readlines()
+    # Python how do I remove the carriage return character from a string
+    for password in passwords:
+      extracted = attempt_to_unzip(filepath, password.rstrip(), extract_location)
+      if extracted:
+        sleep(5)
+        return
+ 
 
 
 def attempt_to_unzip(filepath:str, password: str, extract_location: str):
@@ -13,7 +23,7 @@ def attempt_to_unzip(filepath:str, password: str, extract_location: str):
             f.extractall(extract_location, f.filelist, bytes('password', 'utf-8'))
             f.close()
             print(f'Successfully extracted with password {password}')
-            exit(1)
+            return True
     except: 
         return False
 
