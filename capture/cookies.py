1f827142079c6f1e0b9eb2e0a7f189110b6c629b
Jake Overall <joverall22@gmail.com>
2022-04-26 11:49:30 -0600

    adds cookie grabber

---
+++ capture/cookies.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+
+targetsite = input("Enter target site:") # Uncomment this to accept user input target site
+# targetsite = "http://34.214.194.214//vulnerabilities/xss_d/" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+def bringforthcookiemonster(): # Because why not!
+    print('''
+
+              .---. .---.
+             :     : o   :    me want cookie!
+         _..-:   o :     :-.._    /
+     .-''  '  `---' `---' "   ``-.
+   .'   "   '  "  .    "  . '  "  `.
+  :   '.---.,,.,...,.,.,.,..---.  ' ;
+  `. " `.                     .' " .'
+   `.  '`.                   .' ' .'
+    `.    `-._           _.-' "  .'  .----.
+      `. "    '"--...--"'  . ' .'  .'  o   `.
+
+        ''')
+
+bringforthcookiemonster()
+print("Target site is " + targetsite)
+print(cookie)
+print(f"""
+=============HEADERS============ 
+{response.headers}
+""")
+
+# Add here some code to make this script perform the following:
+# - Send the cookie back to the site and receive a HTTP response
+# - Generate a .html file to capture the contents of the HTTP response
+# - Open it with your browser
